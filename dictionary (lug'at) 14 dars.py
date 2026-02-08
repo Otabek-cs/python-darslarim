0f2044be1e63fc4817d255e4b57de420f9a8cfd2
Otabek-cs <otabekshodiyevt@gmail.com>
2026-02-08 20:45:13 +0700

    yangi dars

---
+++ dictionary (lug'at) 14 dars.py
@@ -0,0 +1,46 @@
+
+
+# 14-dars. Dictionary (lug'atlar)
+# 8.02.2026
+# Muallif: Otabek
+
+#car_0 = {'model': 'ferrari', 'rang': 'oq'}
+#print(car_0['model'])
+#print(car_0['rang'])
+
+# talaba_0 = {'ism':'otabek', 'yosh':'20', 't_yil': 2002}
+# #print(f"{talaba_0['ism'].title()},\
+# #      {talaba_0['t_yil']}-yilda tug'ilgan,\
+#  #         {talaba_0['yosh']} yoshda")
+# talaba_0['kurs'] = 1 
+# talaba_0['fakultet'] = 'Mexanika-matematika'
+# talaba_0['Yo\'nalish'] = 'Matematika va kompyuter science'   
+# print(talaba_0)       
+
+# talaba_1 = {} # bo'sh lug'at
+# talaba_1['ism'] = 'Otabek'
+# talaba_1['kurs'] = 4
+# talaba_1['yosh'] = 22
+# print(talaba_1)
+# print(f"Talaba {talaba_1['ism'].title()} {talaba_1['kurs']} - kurs")
+# talaba_1['kurs'] = 5 # qiymatni o'zgartirish
+# print(f"Talaba {talaba_1['ism'].title()} {talaba_1['kurs']} - kurs")
+
+# mevalar = {'apple': 'olma', 'apricot':'o\'rik'}
+# print(mevalar)
+# del mevalar['apple'] # o'chirish elementni
+# print(mevalar)
+
+# telefonlar = {
+#     'ali':'iphone X',
+#     'vali':'S9',
+#     'otabek':'note 20 ultra',
+#     'ulug\'bek':"iphone 15 pro max"}
+# print(telefonlar.get('ulug\'bek', 'Bunday ism mavjud emas'))
+# # get() metodi agar lug'atda mavjud bo'lmasa none chiqaradi
+
+# A M A L I Y O T
+
+# otam = {'ism':'To\'ra', 't_yil': 1971, 't_joy' : 'Buxoro'}
+# print(f"Otamning ismi {otam['ism'].title()}, {otam['t_yil']} - yilda , {otam['t_joy']} viloyatida tug'ilganlar.")
+
